01b6ae7294642b7c8d7981b0e6e1741c2268ea94
finn_rud <finn.ruderman06@gmail.com>
2026-08-03 13:41:21 -0400

    everything set up
    tested with "Hello world!" and boundaries set to 480x320 to match the screen i'm designing for

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
-//this is a test
+
+
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
